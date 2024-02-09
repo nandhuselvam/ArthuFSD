7b5d653ed1fba97956be77277cc6d6c5efedf4a7
ArthuFSD <nandhuselvam/ArthuFSD>
2024-02-09 17:46:19 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+/*
+open terminal in mac or linux command prompt in windows
+dir 
+cd command 
+cd ..
+cd desktop
+mkdir foldername 
+cd foldernameopen the folder in vs code - either code or open visual code and open the folder
+check the version of the git installed by the 
+   open the terminal
+   git --version
+
+   git remote
+*/
