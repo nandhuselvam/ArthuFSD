aee031db5398ff7917a3aba18f5f05778839fe18
ArthuFSD <nandhuselvam/ArthuFSD>
2024-02-13 14:29:25 +0530

    shopping

---
+++ shopping/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import ProductList from './ProductList'
+import ShoppingCart from './ShoppingCart'
+import Div from './Div'
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems(prevItems => [...prevItems, product]);
+  }
+
+  const handleRemoveFromCart = (productToRemove) => {
+    setCartItems(prevItems => prevItems.filter(product => product !== productToRemove));
+  }
+
+  return (
+    <div className="App">
+      <h1>Shopping Cart</h1>
+      <ProductList onAddToCart={handleAddToCart} />
+      <ShoppingCart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} />
+      <Div></Div>
+    </div>
+  )
+}
+
+export default App
